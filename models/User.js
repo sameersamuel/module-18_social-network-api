3e6554a8cebdc986151ef55bdb86f5eaf7955da6
Sameer B <sameerflyleaf@gmail.com>
2022-10-26 15:31:30 -0700

    models starting

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        unique: true,
+        trim: true,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+      },
+      thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+        }],
+      friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        }],
+    },
+    {
+      toJSON: {
+        getters: true,
+        virtuals: true,
+      },
+    }
+  );
+
+  userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+  });
+const User = model('user', userSchema);
+
+module.exports = User;
